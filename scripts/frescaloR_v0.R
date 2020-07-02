eadb3a5b1ed174f0f2533afc0c788dd758569970
Pescott <olipes@ceh.ac.uk>
2020-07-02 11:41:03 +0100

    save additional benchmark info

--- scripts/frescaloR_v0.R
+++ scripts/frescaloR_v0.R
@@ -6,14 +6,18 @@
 library(useful)
 load(file = "data/unicorns.rda")
 wgts <- read.delim(file = "data/GB_LC_Wts.txt", header = F, sep = "")
+periods <- data.frame(start = c(1980, 1990), end = c(1989, 1999))
+head(periods)
 
 ## Do the data contain locations that have no weights defined? Exclude them.
 dat <- unicorns[unicorns$hectad %in% wgts$V1,] # lost 4 Locations
 missingLocs <- unicorns[!(unicorns$hectad %in% wgts$V1),]
 
-# useful lists
+# useful lists etc.
 uniSpp <- unique(dat$CONCEPT) # unique species list
+sppDF <- data.frame(index = 1:length(uniSpp), species = uniSpp, fLevel = as.numeric(uniSpp))
 uniSites <- unique(dat$hectad) # unique data sites
+siteDF <- data.frame(index = 1:length(uniSites), sites = uniSites)
 siteNWgts <- wgts[wgts$V1 %in% uniSites,] # just keep relevant neighbourhoods
 
 ##########################################
@@ -64,8 +68,9 @@ blmdef <- 0.2703 # default limit for benchmark species
 # lwfL[j,i] # species x site rescaled (and transformed) frequency matrix
 alpha <- rep(1, length(uniSites))
 phi <- rep(0, length(uniSites))
-tot <- tot2 <- spnum <- an2 <- rep(NA, length(uniSites))
-jDat <- jDat2 <- lwfRscd <- lwfRanked <- bench <- matrix(nrow = length(uniSpp), ncol = length(uniSites))
+tot <- tot2 <- spnum <- an2 <- abtot <- rep(NA, length(uniSites))
+jDat <- jDat2 <- lwfRscd <- lwfRanked <- bench <- benchName <- matrix(nrow = length(uniSpp), ncol = length(uniSites))
+siteBench <- list()
 for (i in 1:length(uniSites)) {
   while (isTRUE(abs(phi[i] - phibig) > tol)) {
     for (k in 1:krepmx) { # k iterations
@@ -99,14 +104,36 @@ for (i in 1:length(uniSites)) {
   # Either species where scaled rank < blmdef or where rank = 1 even though scaled rank > blmdef (for small samples)
     #bench[j,i] <- ifelse(lwfRscd[j,i] < blmdef,1,0) # 1 = benchmark
     bench[j,i] <- ifelse(lwfRscd[j,i] < blmdef | rank(lwfRanked[,i])[j] == 1,1,0) # 1 = benchmark
-    }
+    benchName[j,i] <- ifelse(bench[j,i] == 1, sppDF[sppDF$index==j,]$species, 0) # species as factor levels
+  }
+  # Total sum of benchmark spp at sites
+  abtot[i] <- sum(bench[,i])
+  # List of benchmark species' factor levels at each site
+  siteBench[[i]] <- benchName[,i][benchName[,i]!=0] # list of benchmark species (as factor levels) at each site
 }
 #save(alpha, file = "outputs/alpha_v0.0.rda") # save initial versions for quick comparison in vignetteEg.R
 #save(spnum, file = "outputs/spnum_v0.0.rda") # save initial versions for quick comparison in vignetteEg.R
+#save(bench, file = "outputs/bench_v0.0.rda") # save initial versions for quick comparison in vignetteEg.R
 #load(file = "outputs/alpha_v0.0.rda")
 #load(file = "outputs/spnum_v0.0.rda")
+#load(file = "outputs/bench_v0.0.rda")
 
 ##############################################
 ## Translation of fortran subroutine tfcalc ##
 ##############################################
+# Need some species x site x time period arrays for this bit [j,i,t]
+# Following adpated from original Frescalo README
+# sptot - Number of occurrences, given reduced weight of locations having very low sampling effort
+# jtot - Actual number of occurrences of species j at time t
+# esttot - Estimated number of occurrences of species j at time t, given fitted model
+# iocc - 1 if species j is found at location i at time t, 0 otherwise
+# smpint - the benchmark-based sampling intensity at location i and time t
+iocc <- array(dim = c(length(uniSpp), length(uniSites), nrow(periods))) # [j,i,t]
+sampint <- matrix(data = 1.0E-7, nrow = nrow(periods), ncol = length(uniSites)) # [t,i]
+#sampef(i,iit)=sampef(i,iit)+ibench(i,j)*bwght(j)/abtot(i)
+for (t in 1:nrow(periods)) {
+  for (i in 1:length(uniSites)) {
+    sampint[t,i] <- 
+  }
+}
 
